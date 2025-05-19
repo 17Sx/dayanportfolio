007580336346152f723ddeae13c7abd3925bac91
Noa.O <noa.obringer@gmail.com>
2025-05-19 09:27:40 +0200

    🎉  init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        
+    </div>
+  );
+}
+
+export default App;
